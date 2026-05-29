f8efa15cdbf73fc5e221a8578c59982424b2515a
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-05-29 22:43:16 +0530

    ecommerce-product-api

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const productRoutes = require("./routes/product.routes");
+
+app.use(express.json())
+
+app.use("/products", productRoutes)
+
+module.exports = app
