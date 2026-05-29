2c35a6b443a8e164e593cd2b9064052f47d93191
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-05-30 00:06:53 +0530

    ecommerce-product-api

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const {getAllProduct, getProductById} = require("../controllers/product.controller");
+const {getAllProduct, getProductById, createProduct, updateProduct, deleteProduct} = require("../controllers/product.controller");
 
 router.get("/", getAllProduct);
 router.get("/:id", getProductById);
-
-
+router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct)
 module.exports = router;
