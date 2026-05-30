898053704e1ebc14aa3215b7463841a3b84dec1b
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-05-30 23:53:41 +0530

    Error middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
 import connectDB from "./config/db.config.js";
+import errorMiddleware from "./middleware/error.middleware.js";
+
+const app = express();
 
 await connectDB();
 
+app.use(errorMiddleware)
 
-const app = express();
+
 
 export default app;
