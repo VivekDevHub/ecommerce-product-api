404b1612c36cc379ee4d9770fd065a073d9321c5
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-05-30 23:30:27 +0530

    user model creation

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -7,7 +7,7 @@ config();
 const envs = {
   PORT: process.env.PORT,
   MONGODB_URL: process.env.MONGODB_URL,
-  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
   IMAGEKIT_PUBLIC_KEY: process.env.IMAGEKIT_PUBLIC_KEY,
   IMAGEKIT_PRIVATE_KEY: process.env.IMAGEKIT_PRIVATE_KEY,
   IMAGEKIT_URL_ENDPOINT: process.env.IMAGEKIT_URL_ENDPOINT,
