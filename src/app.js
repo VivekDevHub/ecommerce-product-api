27c30366000bb94306c6582c1c2a853688771dd7
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-05-30 23:59:20 +0530

    added error middleware

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 import express from "express";
 import connectDB from "./config/db.config.js";
 import errorMiddleware from "./middleware/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(errorMiddleware);
+app.use(express.json());
+app.use(cookieParser());
+
 await connectDB();
 
-app.use(errorMiddleware)
+
 
 
 
